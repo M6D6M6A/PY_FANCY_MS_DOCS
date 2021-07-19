a06318887742f0b81bfdb17f7d6de9f7afc215c9
Philipp Reuter <reuter.philipp@ymail.com>
2021-07-19 21:06:58 +0200

    ...

--- test/test_empty_excel_file.py
+++ test/test_empty_excel_file.py
@@ -6,6 +6,7 @@ class Test_EmptyExcelFile(unittest.TestCase):
     def setUp(self):
         sys.path.insert(0, "../py_fancy_ms_docs")
         from py_fancy_ms_docs import py_fancy_excel
+        
     def test_str_method_relationships(self):
         test_str_relationships = str(py_fancy_excel.rels())
         original_empty_data = "<Relationships xmlns=\"http://schemas.openxmlformats.org/package/2006/relationships\"><Relationship Id=\"rId3\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles\" Target=\"styles.xml\"/><Relationship Id=\"rId2\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme\" Target=\"theme/theme1.xml\"/><Relationship Id=\"rId1\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet\" Target=\"worksheets/sheet1.xml\"/></Relationships>"
